d6f0c611d520988e53bce06c05764e73ffc0467c
rodeaayush <rodeaayush2@gmail.com>
2025-07-28 19:34:27 +0530

    Jurassic

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ provider "aws" {
 module "ec2"{
   source  = "./ec2"
   vpc_id = module.vpc.vpc_id
-  private-tom = module.vpc.private_subnet_id
-  private-db = module.vpc.private_db_subnet_id
-  public-nginx = module.vpc.public_subnet_id
+  private_tom = module.vpc.private_subnet_id
+  private_db = module.vpc.private_db_subnet_id
+  public_nginx = module.vpc.public_subnet_id
 }
   module "vpc" {
     source = "./vpc"
